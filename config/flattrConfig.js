5058e73c01558cdd70ba96e0c0b02cb770f9fd4f
cloudstrife9494 <aymansmimah@yahoo.fr>
2018-02-16 21:18:57 +0000

    change flattr redirect_uri

--- config/flattrConfig.js
+++ config/flattrConfig.js
@@ -3,7 +3,7 @@ var flattr = require('flattr');
 module.exports.config = {
     client_id: 'qALnzUlXSMC4C4UFvp0kBdtPI3yhm2yW',
     client_secret: 'tMcM6dgXAdThDVIwJxd8rwwTmlecqHzX7nnr7nZ69y2k3pdrhPcGhmTucEAK0j8C',
-    redirect_uri: 'http://localhost:7000/flattr',
+    redirect_uri: 'https://flattrback.herokuapp.com/flattr',
     scope : 'flattr',
     host : 'https://flattr.com/oauth/authorize',
     path : '/oauth/authorize'
